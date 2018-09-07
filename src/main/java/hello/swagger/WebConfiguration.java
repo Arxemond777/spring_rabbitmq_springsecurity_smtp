9c50b36bea19e8991f068b14b14749389c9bd8f1
glushenkov <glushenkov@localhost.localdomain>
2018-09-07 10:53:18 +0300

    swagger has been added

---
+++ src/main/java/hello/swagger/WebConfiguration.java
@@ -0,0 +1,38 @@
+package hello.swagger;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter(
+                new Jackson2ObjectMapperBuilder()
+                        .propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                        .dateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+                        .build()));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
